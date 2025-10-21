ef41e79960ee4c80d379d81813aeec4df870a050
srishti chamilall <srishtichamilall14@gmail.com>
2025-10-21 21:01:21 +0400

    Routing, Controller, Views for Lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +10,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # Read all
+  get "lists", to: "lists#index"
+  # Create
+  get "lists/new", to: "lists#new",  as: :new_list
+  post "lists", to: "lists#create"
+  # Read one - The `show` route needs to be *after* `new` route.
+  get "lists/:id", to: "lists#show", as: :list
 end
